661f8360b015701b8b9f7d7aa657827070ded30f
Malvin Opondo <malvinopondo@gmail.com>
2023-10-10 08:34:29 +0300

    feat(Dependency): Install react icons

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-        <h1 className='text-white font-bold'>REACT.</h1>
+    <div className='flex justify-between items-center h-24 max-w-[1040px] mx-auto px-2'>
+        <h1 className='text-white text-3xl w-full font-bold'>KHALISI .</h1>
+        <ul className='flex text-white'>
+          <li className='p-5'>Home</li>
+          <li className='p-5'>Company</li>
+          <li className='p-5'>Resources</li>
+          <li className='p-5'>About</li>
+          <li className='p-5'>Contact</li>
+        </ul>
     </div>
   )
 }
