1206b5c3efd41365a339d4819238a81e8e62a812
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2023-10-21 21:09:33 +0300

    feat(Home): Add a react-typed dependency

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div className='text-white'>
-        <div className='max-w-[800px] mt-96px w-full h-screen mx-auto text-center flex flex-col justify-center'>
-        <p className='text[]'>GROWING WITH DESIGN METRICS</p>
-        <h1>Grow with design.</h1>
-        <p>
-            Creative, Affordable & Reliable
+    <div className="text-white">
+      <div className="max-w-[800px] mt[-96px] w-full h-screen mx-auto text-center flex-col justify-center">
+        <p className="text-[yellow] text-lg p-40 font-bold">
+          <h1 className="md:text-7xl sm:text-4xl text-3xl font-bold md:py-6">GROWING WITH <span className="text-white">DESIGN METRICS</span></h1>
         </p>
-
+        <p className="md:text-4xl font-bold sm:text-xl">Grow with design</p>
+        <div>
+        <p>Creative, Affordable & Reliable</p>
         </div>
-
+        
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,7 @@ import {FiMenu} from "react-icons/fi"
 function Navbar() {
   return (
     <div className='flex justify-between items-center h-24 max-w-[1040px] mx-auto px-2'>
-        <h1 className='text-white text-3xl w-full font-bold'>KHALISI .</h1>
+        <h1 className='text-yellow-500 text-3xl w-full font-bold'>KHALISI .</h1>
         <ul className='flex text-white'>
           <li className='p-5'>Home</li>
           <li className='p-5'>Company</li>
