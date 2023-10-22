526d6a6fdc9e9abd67f32d3d221a375d1382df61
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2023-10-22 15:57:14 +0300

    feat(Design): Import the design vector into assets folder

--- src/Components/Design.js
+++ src/Components/Design.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Design() {
   return (
     <div className='w-full bg-white py-16 px-4'>
-        <div className="max-w-[1200px] mx-auto md:grid-cols-2">
+        <div className="max-w-[1240px] mx-auto md:grid-cols-2">
 
         </div>
 
