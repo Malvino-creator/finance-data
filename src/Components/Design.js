957dcd96ef8163b4b8480085469776da0e8acb1f
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2023-10-22 16:22:03 +0300

    feat(Design): Add a paragraph to the design compnent

--- src/Components/Design.js
+++ src/Components/Design.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import designer from "../../src/assets/designer.jpg"
 
 function Design() {
   return (
     <div className='w-full bg-white py-16 px-4'>
         <div className="max-w-[1240px] mx-auto md:grid-cols-2">
-
+            <img src={designer} alt='designer'/>
+            <div>
+                <p>DESIGN WITH PURPOSE</p>
+                <h1>Customize your Designs Passionately</h1>
+                <p>Our top most priority at Khalisi is to stand out, captivate, and inspire. 
+                Our graphic services breathe life into your vision, elevating your brand and making it unforgettable.
+                Ready to take your brand to the next level? 
+                Contact us today for a free consultation and discover how our exceptional graphic design can make a difference for your business.</p>
+            </div>
         </div>
 
     </div>
