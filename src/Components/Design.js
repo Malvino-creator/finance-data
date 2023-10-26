da3111d8ae2d307c4957efcab7704bcb6c34f22e
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2023-10-26 09:26:50 +0300

    feat(Design): Add styling to this component

--- src/Components/Design.js
+++ src/Components/Design.js
@@ -6,11 +6,11 @@ function Design() {
     <div className="w-full bg-white py-16 px-4">
       <div className="max-w-[1140px] mx-auto md:grid-cols-2">
         <div>
-          <img className="object-fill h-58 w-96 ..." src={designer} alt="designer" />
+          <img className="w-[600px] mx-auto my-2" src={designer} alt="designer" />
         </div>
-        <div className="text-black font-semibold">
-          <p>DESIGN WITH PURPOSE</p>
-          <h1>Customize your Designs Passionately</h1>
+        <div className="text-black flex flex-col justify-center font-semibold">
+          <p className="text-[yellow] font-bold">DESIGN WITH PURPOSE</p>
+          <h1 className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">Customize your Designs Passionately</h1>
           <p>
             Our top most priority at Khalisi is to stand out, captivate, and
             inspire. Our graphic services breathe life into your vision,
@@ -19,6 +19,7 @@ function Design() {
             and discover how our exceptional graphic design can make a
             difference for your business.
           </p>
+          <button className="bg-[black] text-[white] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3 ">Get started</button>
         </div>
       </div>
     </div>
